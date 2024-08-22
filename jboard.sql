a5689605273b2d12ceedb312d50ef2df2067ed75
repos8646 <repos8646@gmail.com>
2024-08-22 19:29:31 +0900

    jboard sql plus 0822

--- jboard.sql
+++ jboard.sql
@@ -73,3 +73,27 @@ SELECT COUNT(*) FROM `user` WHERE `uid`='abc123';
 SELECT SHA2('1234', 256);
 
 select * from `user` where `uid`='abc101' and `pass`=SHA2('zxcv123!', 256);
+
+SELECT MAX(`no`) FROM `article`;
+
+SELECT * FROM `article` AS a 
+left JOIN `file` AS b ON a.`no`=b.ano 
+WHERE `no`=8; 
+
+SELECT a.*, b.nick FROM `article` AS a
+JOIN `user` AS b
+ON a.writer = b.uid
+ORDER BY `no` DESC
+LIMIT 10;
+
+INSERT INTO `article` (`title`, `content`, `writer`, `regip`, `rdate`) SELECT `title`, `content`, `writer`, `regip`, `rdate` FROM `article`;
+
+SELECT COUNT(*) FROM `article`;
+
+DELETE FROM `article` WHERE `no` > 100;
+
+SELECT * FROM `article` AS a
+left JOIN `file` AS b ON a.`no`=b.ano
+WHERE `no`='1';
+
+update file set `download` = `download` + 1 where `fno`=2;
